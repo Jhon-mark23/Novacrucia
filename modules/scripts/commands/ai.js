f2f6dd969fb3a78c3e37499917983cb8e48c34ce
Mark <123864853+Jhon-mark23@users.noreply.github.com>
2025-01-13 19:35:16 +0800

    Update ai.js

--- modules/scripts/commands/ai.js
+++ modules/scripts/commands/ai.js
@@ -1,13 +1,19 @@
 const axios = require("axios");
 const { sendMessage } = require('../handles/sendMessage');
 
-module.exports = {
-  name: "ai",
-  description: "Gpt4o x Gemini AI",
-  role: 1,
-  author: "Kiana",
 
-  async execute(bot, args, authToken, event) {
+module.exports.config = {
+    name: "testreplydata", // Command name
+    author: "mark",
+    version: "1.0",
+    category: "test",
+    description: "Get Reply Message Data",
+    adminOnly: false, // if false all users can use this command but if true only the admin id lists on config.json can use
+    usePrefix: true, // if true it uses the command with the prefix example /testsendimage-url but if false no need prefix can direct use testsendimage-url
+    cooldown: 5, // cooldown on how many command to be used
+};
+
+module.exports.run = async function ({ bot, args, authToken, event }) {
     if (!event?.sender?.id) {
       console.error('Invalid event object: Missing sender ID.');
       sendMessage(bot, { text: 'Error: Missing sender ID.' }, authToken);
